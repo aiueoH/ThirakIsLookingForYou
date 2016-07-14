d737a720ba5a02e03080b13bd72c8c80ed34f1f6
aiueoH@Cosair <aiueo.huang.github@gmail.com>
2016-07-14 15:47:03 +0800

    Init : Can generate notification.

---
+++ app/src/main/java/com/dowob/thirakislookingforyou/MainActivity.java
@@ -0,0 +1,56 @@
+package com.dowob.thirakislookingforyou;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "sent notification");
+                sendNotification("from", "msg");
+            }
+        });
+
+        startService(new Intent(this, NLService.class));
+    }
+
+    private void sendNotification(String from, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("this is title TTT")
+                .setContentText("this is msg MMM")
+                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
