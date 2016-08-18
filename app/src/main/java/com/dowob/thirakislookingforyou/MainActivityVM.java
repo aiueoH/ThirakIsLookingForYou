127a3d2e1fd8f6c49f83e4c163f78c68e8528861
Wayne <wayne.huang@kdanmobile.com>
2016-08-18 15:22:01 +0800

    Add console textview.(Observable sample)

--- app/src/main/java/com/dowob/thirakislookingforyou/MainActivityVM.java
+++ app/src/main/java/com/dowob/thirakislookingforyou/MainActivityVM.java
@@ -1,9 +1,13 @@
 package com.dowob.thirakislookingforyou;
 
 import android.content.Context;
+import android.content.EntityIterator;
 import android.content.Intent;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.os.Environment;
 import android.view.View;
+import com.dowob.thirakislookingforyou.BR;
 
 import java.util.Calendar;
 
@@ -13,14 +17,15 @@ import java.util.Calendar;
 public class MainActivityVM extends BaseObservable {
     private Context mContext;
 
-    private String mButtonText = "this is button text from VM";
+    private String mConsole = "";
+    private String mSendNotificationButtonText = "Send notification";
 
     public MainActivityVM(Context context) {
         mContext = context;
         context.startService(new Intent(mContext, NLService.class));
     }
 
-    public void onClickButton(View view) {
+    public void onClickSendNotificationButton(View view) {
         sendNotification();
     }
 
@@ -28,9 +33,20 @@ public class MainActivityVM extends BaseObservable {
         String title = "TestNotification " + Calendar.getInstance().getTime().getMinutes() + " " + Calendar.getInstance().getTime().getSeconds();
         String text = "Hello! This is test notification.";
         NotificationSender.send(mContext, title, text);
+        updateConsole(title, text);
     }
 
-    public String getButtonText() {
-        return mButtonText;
+    private void updateConsole(String title, String text) {
+        mConsole += "\n" + title + "\n" + text;
+        notifyPropertyChanged(BR.console);
+    }
+
+    public String getSendNotificationButtonText() {
+        return mSendNotificationButtonText;
+    }
+
+    @Bindable
+    public String getConsole() {
+        return mConsole;
     }
 }
